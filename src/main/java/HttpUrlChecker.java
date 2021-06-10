aaf4da5f279ddfda439f6751e04c7891ed39c3cb
alessandrodetta <alessandro.detta3@gmail.com>
2021-06-10 12:12:33 +0200

    Upload Finished exercise

--- src/main/java/HttpUrlChecker.java
+++ src/main/java/HttpUrlChecker.java
@@ -21,3 +21,12 @@
  * PROVIDED HEREUNDER IS PROVIDED "AS IS". HTBLA LEONDING HAS NO OBLIGATION
  * TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
  */
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class HttpUrlChecker{
+    public static Matcher getMatcher(String httpString){
+        Pattern pattern = Pattern.compile("^((http)s?://)(www\\.)?([a-z]|[A-Z]|[0-9])+(-([a-z]|[A-Z]|[0-9])+)*(\\.[a-z]+)+$");
+        return pattern.matcher(httpString);
+    }
+}
